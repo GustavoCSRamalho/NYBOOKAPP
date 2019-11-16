84865c1c0bc98f557b0c1c17451e74578b227a5e
Gustavo Ramalho <gstvramalho@gmail.com>
2019-11-15 23:03:59 -0300

    New base

---
+++ app/src/main/java/gustavo/com/nyapp/presentation/books/BooksActivity.kt
@@ -0,0 +1,15 @@
+package gustavo.com.nyapp.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import gustavo.com.nyapp.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+
+
+    }
+}
